60912f3f7d40f4863ef5448e4fe59b1c46f87242
Tahneet Kanwal <99545467+tahneetK13@users.noreply.github.com>
2023-10-23 21:31:53 +0530

    Update ArrayShuffler.java

--- 1. Java/ArrayShuffler.java
+++ 1. Java/ArrayShuffler.java
@@ -1,1 +1,24 @@
+// Create an array with the values(1,2,3,4,5,6,7) and shuffle it.
+import java.util.Random;
+
+public class ArrayShuffler {
+    public static void main(String[] args) {
+        int[] originalArray = {1, 2, 3, 4, 5, 6, 7};
+        shuffleArray(originalArray);
+        System.out.print("Shuffled Array: ");
+        for (int num : originalArray) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void shuffleArray(int[] array) {
+        Random rand = new Random();
+        for (int i = array.length - 1; i > 0; i--) {
+            int index = rand.nextInt(i + 1);
+            int temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
+        }
+    }
+}
 
